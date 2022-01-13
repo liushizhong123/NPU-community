c6b2422793e7f4a3de93437740f6703384061d28
lsz <lsz@123.cn>
2022-01-13 17:34:13 +0800

    邮件发送 commit

---
+++ src/main/java/com/nowcoder/community/util/MailClient.java
@@ -0,0 +1,45 @@
+package com.nowcoder.community.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * 发送邮件工具类
+ *
+ * @author lsz on 2022/1/13
+ */
+@Component
+public class MailClient {
+    // 日志
+    private static final Logger logger = LoggerFactory.getLogger(MailClient.class);
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    // 发送邮件
+    public void sendMail(String to, String subject, String content) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setFrom(from); // 发件人
+            helper.setTo(to); // 发送给谁
+            helper.setSubject(subject); // 发送的主题
+            helper.setText(content, true); // 允许发送html字符串
+            mailSender.send(helper.getMimeMessage());
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            logger.error("发送邮件失败：" + e.getMessage());
+        }
+    }
+}
--- src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -1,13 +1,32 @@
 package com.nowcoder.community;
 
+import com.nowcoder.community.util.MailClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @SpringBootTest
 class CommunityApplicationTests {
 
+    @Autowired
+    private MailClient mailClient;
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Test
-    void contextLoads() {
+    void testMailSend() {
+        mailClient.sendMail("1013552449@qq.com", "这是测试SpringBoot发普通内容邮件的功能", "你好帅!!!");
+    }
+
+    @Test
+    public void testHtmlMail(){ // 尝试发送HTML邮件
+        Context context = new Context(); // thymeleaf 的 Context
+        context.setVariable("username", "Jame Liu");
+        String content = templateEngine.process("/mail/demo", context); // 生成动态网页
+        System.out.println(content);
+        mailClient.sendMail("1013552449@qq.com", "这是测试SpringBoot发HTML邮件的功能", content);
     }
 
 }
